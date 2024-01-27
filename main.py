86df7fe4767dfc2b87a8608fad87ef7f60f94776
Ioannis Simitas <ioannissimitasd06@gmail.com>
2024-01-27 12:22:54 +0200

    Base

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ import pymongo
 
 def main():
     print('Welcome to the School Adminstration System')
-    choices = input('1) Create Student\nChoose one of the available options: ')
+    choices = input('1) Create Student\n2) Read Student Data\nChoose one of the available options: ')
 
 
 
+    student = Students(name, age, phone, email, fathers_name, amka)
+    
     if choices == '1':
         name = input('Add students name: ')
         age = input('Add students age: ')
         phone = input('Add students phone number: ')
         email = input('Add students email: ')
         fathers_name = input('Add fathers name: ')
+    
         amka = input('Add students amka: ')
-
-
-        
-        student = Students(name, age, phone, email, fathers_name, amka)
         student.create_student()
+    
+    elif choices == '2':
+        student.read_student_data()
 
 main()
--- student.py
+++ student.py
@@ -5,17 +5,18 @@ class Students:
         self.connect_to_database = pymongo.MongoClient('mongodb://localhost:27017/')
         self.name = name
         self.age = age
+        
         self.pn = pn
-
         self.email = email
         self.fathers_name = fathers_name
         self.amka = amka
 
     def create_student(self):
+        global db
+        global data
         db = self.connect_to_database['school']
+       
         data = db['students']
-
-
 
         student_data = {
             'Students Name': self.name,
@@ -24,10 +25,11 @@ class Students:
             'Students Email': self.email,
             'Students Fathers Name': self.fathers_name,
             'Students AMKA': self.amka
+
         }
 
 
         save_to_database = data.insert_one(student_data)
 
-def read_student_data(self):
-    pass
+    def read_student_data(self):
+        db.students.find()
